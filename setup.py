25ed5b11332c444659fc3a33b912ab3e5e083f76
Wes Ladd <wesladd@traingrc.com>
2023-08-18 16:31:07 -0500

    updated working copy

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='textweaver',
-    version='0.0.5',
+    version='0.0.7',
     author='Wes Ladd',
     author_email='wesladd@traingrc.com',
     description='A FastAPI-based web server for working with LLMs, embedding models, and PostgresSQL.',
--- weaver/routers/search.py
+++ weaver/routers/search.py
@@ -14,6 +14,10 @@ top_k = 5  # Default value; adjust as needed
 class SearchRequest(BaseModel):
     query: str
     results_to_return: int = top_k  # Optional parameter with a default value
+    search_type: str = "euclidean"  # New optional parameter with default value
+    probes: int = 10  # Optional parameter for number of probes, defaults to 1
+    workers: int = 1  # Optional parameter for number of workers, defaults to 1
+
 
 @router.post("/search")
 def search(request: SearchRequest):
@@ -30,7 +34,7 @@ def search(request: SearchRequest):
         curl 'http://127.0.0.1:8000/search' \
              -X 'POST' \
              -H 'Content-Type: application/json' \
-             --data '{"query": "what is malware", "results_to_return": 10}'
+             --data '{"query": "string", "results_to_return": 5, "search_type": "euclidean", "probes": 100}'
 
     Returns:
         dict: A dictionary containing the search results and the time taken to fetch them. 
@@ -53,25 +57,43 @@ def search(request: SearchRequest):
     """
     connection = get_connection()
     cursor = connection.cursor()
+    probes = request.probes
     query = request.query
+    workers = request.workers
     results_to_return = request.results_to_return
     query_vector = model.encode([[instruction, query]])[0].tolist()
-    sql_query = """SELECT filename, metadata, embeddings_text, embeddings <-> %s::vector AS distance
+    search_type = request.search_type
+    
+    # Validate and get the corresponding operator
+    operator_mapping = {
+        "euclidean": "<->",
+        "inner_product": "<#>",
+        "cosine": "<=>"
+    }
+    operator = operator_mapping.get(search_type, "<->")  # Default to euclidean if an invalid value is provided
+    sql_query = f"""SELECT filename, metadata, embeddings_text, embeddings {operator} %s::vector AS distance
                    FROM embeddings
                    ORDER BY distance
                    LIMIT %s"""
 
     try:
-        start_time = time.time()
+        start_time = time.time()        
+        cursor.execute("BEGIN;")
+        cursor.execute(f"SET LOCAL ivfflat.probes = {probes};")
+        cursor.execute(f"SET LOCAL max_parallel_workers_per_gather = {workers};")
         cursor.execute(sql_query, (query_vector, results_to_return))
         top_results = cursor.fetchall()
         end_time = time.time()
         time_elapsed = round(end_time - start_time, 2)
-        
+        cursor.execute("COMMIT;")
         results = []
         for result in top_results:
             filename, metadata, embeddings_text, similarity_score = result
             json_header_line, text_body = embeddings_text.split('\n', 1)
+            if search_type == "cosine":
+                similarity_score = 1 - similarity_score
+            elif search_type == "inner_product":
+                similarity_score = abs(similarity_score)
             # Extract the Title and Link from the metadata
             links = metadata.get("links", [])
             for link_info in links:
