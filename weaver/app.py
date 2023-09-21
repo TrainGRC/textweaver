4f746b51ecddd1c64e56adc31bbc791c04dd7216
Wesley ladd <wesladd@traingrc.com>
2023-09-20 21:06:45 -0500

    updated

--- weaver/app.py
+++ weaver/app.py
@@ -71,7 +71,7 @@ app.include_router(upload.router)
 
 @app.get("/health")
 def read_health():
-    return {"status": "up"}
+    return {"status": "healthy"}
 
 @app.on_event("startup")
 def startup_event():
