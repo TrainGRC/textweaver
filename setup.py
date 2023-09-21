bff2402793e6b83bdd7db01ea98be2d732504802
Wesley ladd <wesladd@traingrc.com>
2023-09-20 19:17:00 -0500

    updated to fix synthetics canary execution role permissions

--- setup.py
+++ setup.py
@@ -70,5 +70,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
+        'Topic :: Text Processing :: Linguistic',
     ],
 )
